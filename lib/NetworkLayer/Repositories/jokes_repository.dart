3545540f8a1e7c8187c3699efe099a60ef8692c0
Vijay Bhaskar G <vijaymca.galla@gmail.com>
2024-11-04 18:37:06 +0530

    Final changes

--- lib/NetworkLayer/Repositories/jokes_repository.dart
+++ lib/NetworkLayer/Repositories/jokes_repository.dart
@@ -5,11 +5,15 @@ import 'package:http/http.dart';
 import 'dart:convert';
 
 class JokesRepository {
+  late NetworkApi _networkApi;
+  JokesRepository() {
+    _networkApi = NetworkApi();
+  }
+
   // api to fetch random joke for the first time and when search is empty
   Future<List<Result>> getRandomJoke() async {
-    final NetworkApi networkApi = NetworkApi();
     try {
-      Response response = await networkApi.getData(randomJokeApi);
+      Response response = await _networkApi.getData(randomJokeApi);
       Map<String, dynamic> data = jsonDecode(response.body);
       return [Result.fromJson(data)];
     } catch (exception) {
@@ -19,9 +23,8 @@ class JokesRepository {
 
 // api to fetch jokes based on the search query
   Future<List<Result>> getSearchJokes(String query) async {
-    final NetworkApi networkApi = NetworkApi();
     try {
-      Response response = await networkApi.getData("$searchJokeApi$query");
+      Response response = await _networkApi.getData("$searchJokeApi$query");
       Map<String, dynamic> data = jsonDecode(response.body);
       final searchData = JokeSearchModel.fromJson(data);
       return searchData.results ?? [];
